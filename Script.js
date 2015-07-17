8d16d6d6fb4b284b33304e5fabe17c9680cd08ac
FTdiscovery <gordonchi11@gmail.com>
2015-07-17 17:26:55 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -207,21 +207,6 @@ function Game() {
 
 	if (VaccinesGiven >= People) {
 		alert("You have saved all the people of the world! e1em is now going to spread to every single person on earth...but do not worry! Everyone has been vaccined and is safe! Congratulations, you have won the game!");
-		var name = prompt("What is your goal?");
-		var quest = prompt("What is your quest?");
-		var res = quest.split(" ");
-		var goodPersonIsCool = false;
-		for (var i = 0; i < res.length; i++) {
-			if (res[i] == "e1em") {
-				goodPersonIsCool = true;
-			}
-		}
-		if (!goodPersonIsCool) {
-			var adjs = ["save", "destroy", "digest"];
-			var which = Math.floor(Math.random() * 3);
-			alert("Your priorities are not set straight. We're trying to " + adjs[which] + "here.");
-			window.close();
-		}
 		endGame = 1;
 		
 		}
@@ -314,3 +299,19 @@ function Pause() {
 alert("Welcome to the Stop e1em Game! You have 120,000 minutes in gametime to save all 7 billion people on earth from e1em! Any person you don't give vaccine to will die once the time is up! Good luck! Save the world from e1em! You start with $2000. Use it wisely.");
 alert("E1EM is a dangerous virus and is an abbreviation for Enthusiastic 1 Expordelli Maximus. The disease will spread to every single person in 120,000 minutes in gametime and will kill every single person that does not receive a vaccine from that time.");
 alert("BUY VACCINES. GIVE VACCINES. REPEAT. HAVE FUN OK BYE")
+
+var name = prompt("What is your name?");
+		var quest = prompt("What is your quest?");
+		var res = quest.split(" ");
+		var goodPersonIsCool = false;
+		for (var i = 0; i < res.length; i++) {
+			if (res[i] == "e1em") {
+				goodPersonIsCool = true;
+			}
+		}
+		if (!goodPersonIsCool) {
+			var adjs = ["save", "destroy", "digest"];
+			var which = Math.floor(Math.random() * 3);
+			alert("Your priorities are not set straight. We're trying to " + adjs[which] + "here.");
+			window.close();
+		}
